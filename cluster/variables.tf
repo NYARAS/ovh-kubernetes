d27bb3181c19c71ee1c6fcb4d02d8eb2a420a8fa
Calvine Otieno <calvineotieno010@gmail.com>
2025-02-11 22:35:28 +0300

    feat: added cluster config 🔥
    Signed-off-by: Calvine Otieno <calvineotieno010@gmail.com>

---
+++ cluster/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  description = "OVH region from https://www.ovhcloud.com/en/public-cloud/regions-availability/"
+  default     = "DE1"
+}
+
+variable "project_id" {
+  description = "OVH project ID"
+  default = ""
+}
+
+variable "name_prefix" {
+  type        = string
+  description = "Prefix to be used on each infrastructure object Name created in OVH."
+  default = "nsw"
+}
+
+variable "private_network" {
+  
+}
