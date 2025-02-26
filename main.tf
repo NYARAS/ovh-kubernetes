fae32eacec6d4d6cfdf251d5cccf4fa15682aa27
Calvine Otieno <nyarangaotieno@gmail.com>
2025-02-26 22:18:43 +0300

    chore: added nodepool creation
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ module "cluster" {
   private_network = module.vpc.private_network
 }
 
+module "node_pool_untainted" {
+  source          = "./nodepool_untainted"
+  clusterName     = module.cluster.clusterName
+  clusterId       = module.cluster.clusterId
+  purpose         = "general-services"
+  suffix          = "b2-15"
+  max_nodes       = 2
+  min_nodes       = 1
+  desired_nodes   = 1
+  private_network = module.vpc.private_network
+}
